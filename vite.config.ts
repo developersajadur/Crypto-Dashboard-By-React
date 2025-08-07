6b58e2d8f9a3e7234c258d9a2a85b5a7d64929d5
developersajadur <itzmesojib@gmail.com>
2025-08-08 01:33:03 +0600

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "./",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: false, 
   },
 });
